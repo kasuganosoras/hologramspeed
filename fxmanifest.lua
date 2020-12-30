63dc1f5b104a0955fe5f0f959c5d7461e2726e23
Jaymo <jaymo.averay@gmail.com>
2020-12-30 15:39:36 +1030

    delete useless server script

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -19,11 +19,6 @@ client_scripts {
 	'client.lua',
 }
 
-server_scripts {
-	'config.lua',
-	'server.lua',
-}
-
 data_file 'HANDLING_FILE' 'data/handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
