b479169db83964fc73751065e80f749254879368
Akkariin Meiko <akkariins@gmail.com>
2020-12-31 20:58:26 +0800

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,22 +1,25 @@
+-- Events
+RegisterNetEvent('HologramSpeed:SetTheme')
+
 -- Constants
-local ResourceName = GetCurrentResourceName()
-local HologramURI = string.format("nui://%s/ui/hologram.html", ResourceName)
-local AttachmentOffset = vec3(2.5, -1, 0.85)
+local ResourceName       = GetCurrentResourceName()
+local HologramURI        = string.format("nui://%s/ui/hologram.html", ResourceName)
+local AttachmentOffset   = vec3(2.5, -1, 0.85)
 local AttachmentRotation = vec3(0, 0, -15)
-local HologramModel = `hologram_box_model`
-local UpdateFrequency = 0 -- If less than average frame time, there will be an update every tick regardless of the actual number specified.
-local ThemeSettingKey = string.format("%s:theme", GetCurrentServerEndpoint()) -- The key to store the current theme setting in. As themes are per server, this key is also.
-local DBG = false -- Enables debug information, not very useful unless you know what you are doing!
+local HologramModel      = `hologram_box_model`
+local UpdateFrequency    = 0 -- If less than average frame time, there will be an update every tick regardless of the actual number specified.
+local SettingKey         = string.format("%s:profile", GetCurrentServerEndpoint()) -- The key to store the current theme setting in. As themes are per server, this key is also.
+local DBG                = false -- Enables debug information, not very useful unless you know what you are doing!
 
 -- Variables
-local duiObject = false -- The DUI object, used for messaging and is destroyed when the resource is stopped
-local duiIsReady = false -- Set by a callback triggered by DUI once the javascript has fully loaded
+local duiObject      = false -- The DUI object, used for messaging and is destroyed when the resource is stopped
+local duiIsReady     = false -- Set by a callback triggered by DUI once the javascript has fully loaded
 local hologramObject = 0 -- The current DUI anchor. 0 when one does not exist
 local usingMetric, shouldUseMetric = ShouldUseMetricMeasurements() -- Used to track the status of the metric measurement setting
 
 -- Preferences
-local displayEnabled = GetResourceKvpInt("displayEnabled") ~= 0
-local currentTheme = GetResourceKvpString(ThemeSettingKey) or GetConvar("hsp_defaultTheme", "default")
+local displayEnabled = false
+local currentTheme   = GetConvar("hsp_defaultTheme", "default")
 
 local function DebugPrint(...)
 	if DBG then
@@ -43,10 +46,31 @@ RegisterNUICallback("duiIsReady", function(_, cb)
     cb({ok = true})
 end)
 
+local function LoadPlayerProfile()
+	local jsonData = GetResourceKvpString(SettingKey)
+	if jsonData ~= nil then
+		jsonData           = json.decode(jsonData)
+		displayEnabled     = jsonData.displayEnabled
+		currentTheme       = jsonData.currentTheme
+		AttachmentOffset   = vec3(jsonData.attachmentOffset.x, jsonData.attachmentOffset.y, jsonData.attachmentOffset.z)
+		AttachmentRotation = vec3(jsonData.attachmentRotation.x, jsonData.attachmentRotation.y, jsonData.attachmentRotation.z)
+	end
+end
+
+local function SavePlayerProfile()
+	local jsonData = {
+		displayEnabled     = displayEnabled,
+		currentTheme       = currentTheme,
+		attachmentOffset   = AttachmentOffset,
+		attachmentRotation = AttachmentRotation,
+	}
+	SetResourceKvp(SettingKey, json.encode(jsonData))
+end
+
 local function ToggleDisplay()
 	displayEnabled = not displayEnabled
 	SendChatMessage("Holographic speedometer " .. (displayEnabled and "^2enabled^r" or "^1disabled^r") .. ".") 
-	SetResourceKvpInt("displayEnabled", displayEnabled and 1 or 0)
+	SavePlayerProfile()
 end
 
 local function SetTheme(newTheme)
@@ -54,20 +78,95 @@ local function SetTheme(newTheme)
 		EnsureDuiMessage {theme = newTheme}
 		SendChatMessage(newTheme == "default" and "Holographic speedometer theme ^5reset^r." or ("Holographic speedometer theme set to ^5" .. newTheme .. "^r."))
 		currentTheme = newTheme
-		SetResourceKvp(ThemeSettingKey, newTheme)
+		SavePlayerProfile()
 	end
 end
+
+local function UpdateEntityAttach()
+	local playerPed, currentVehicle
+	playerPed = PlayerPedId()
+	if IsPedInAnyVehicle(playerPed) then
+		currentVehicle = GetVehiclePedIsIn(playerPed, false)
+		-- Attach the hologram to the vehicle
+		AttachEntityToEntity(hologramObject, currentVehicle, GetEntityBoneIndexByName(currentVehicle, "chassis"), AttachmentOffset, AttachmentRotation, false, false, false, false, false, true)
+		DebugPrint(string.format("DUI anchor %s attached to %s", hologramObject, currentVehicle))
+	end
+end
+
+local function CheckRange(x, y, z, minVal, maxVal)
+	if x == nil or y == nil or z == nil or minVal == nil or maxVal == nil then
+		return false
+	else
+		return not (x < minVal or x > maxVal or y < minVal or y > maxVal or z < minVal or z > maxVal)
+	end
+end
+
+-- Command Handler
+
+local function CommandHandler(args)
+
+	local msgErr = "^1The acceptance range of ^0%s ^1is ^0%f^1 ~ ^0%f^1, reset to default setting.^r"
+	local msgSuc = "^2Speedometer ^0%s ^2change to ^0%f, %f, %f^r"
+	
+	if args[1] == "theme" then
+		if #args >= 2 then
+			TriggerServerEvent('HologramSpeed:CheckTheme', args[2])
+		else
+			SendChatMessage("^1Invalid theme! ^0Usage: /hsp theme <name>^r")
+		end
+	elseif args[1] == "offset" then
+		local nx, ny, nz = 2.5, -1, 0.85
+		if #args >= 4 then
+			nx, ny, nz = tonumber(args[2]), tonumber(args[3]), tonumber(args[4])
+			if not CheckRange(nx, ny, nz, -5.0, 5.0) then
+				nx, ny, nz = 2.5, -1, 0.85
+				SendChatMessage(string.format(msgErr, args[1], -5.0, 5.0))
+			end
+		else
+			SendChatMessage("Reset to default offset, to change the offset, use: /hsp offset <X> <Y> <Z>")
+		end
+		AttachmentOffset = vec3(nx, ny, nz)
+		UpdateEntityAttach()
+		SavePlayerProfile()
+		SendChatMessage(string.format(msgSuc, args[1], nx, ny, nz))
+	elseif args[1] == "rotate" then
+		local nx, ny, nz = 0, 0, -15
+		if #args >= 4 then
+			nx, ny, nz = tonumber(args[2]), tonumber(args[3]), tonumber(args[4])
+			if not CheckRange(nx, ny, nz, -45.0, 45.0) then
+				nx, ny, nz = 0, 0, -15
+				SendChatMessage(string.format(msgErr, args[1], -45.0, 45.0))
+			end
+		else
+			SendChatMessage("Reset to default rotate, to change the rotate, use: /hsp rotate <X> <Y> <Z>")
+		end
+		AttachmentRotation = vec3(nx, ny, nz)
+		UpdateEntityAttach()
+		SavePlayerProfile()
+		SendChatMessage(string.format(msgSuc, args[1], nx, ny, nz))
+	else
+		SendChatMessage("^1Usage: ^0/hsp <theme|offset|rotate> [args...]^r")
+	end
+end
+
+-- Network events
+
+AddEventHandler('HologramSpeed:SetTheme', function(theme)
+	SetTheme(theme)
+end)
+
+-- Register command
 
 RegisterCommand("hsp", function(_, args)	
 	if #args == 0 then
 		ToggleDisplay()
 	else
-		SetTheme(tostring(args[1]))
+		CommandHandler(args)
 	end
 end, false)
 
 TriggerEvent('chat:addSuggestion', '/hsp', 'Toggle the holographic speedometer', {
-    { name = "theme", help = "Set the theme of the speedometer.\n\"default\" is the default theme." },
+    { name = "command",  help = "Allow command: theme, offset, rotate" },
 })
 
 RegisterKeyMapping("hsp", "Toggle Holographic Speedometer", "keyboard", "grave") -- default: `
@@ -92,8 +191,8 @@ local function InitialiseDui()
 
 	DebugPrint("\tDUI initialised")
 
-	local txdHandle = CreateRuntimeTxd("HologramDUI")
-	local duiHandle = GetDuiHandle(duiObject)
+	local txdHandle  = CreateRuntimeTxd("HologramDUI")
+	local duiHandle  = GetDuiHandle(duiObject)
 	local duiTexture = CreateRuntimeTextureFromDuiHandle(txdHandle, "DUI", duiHandle)
 	DebugPrint("\tRuntime texture created")
 
@@ -120,7 +219,9 @@ CreateThread(function()
 		SendChatMessage("^1Could not find `hologram_box_model` in the game... ^rHave you installed the resource correctly?")
 		return
 	end
-
+	
+	LoadPlayerProfile()
+	
 	InitialiseDui()
 
 	-- This thread watches for changes to the user's preferred measurement system
@@ -176,11 +277,11 @@ CreateThread(function()
 						vehicleSpeed = GetEntitySpeed(currentVehicle)
 
 						EnsureDuiMessage {
-							display = displayEnabled and IsVehicleEngineOn(currentVehicle),
-							rpm = GetVehicleCurrentRpm(currentVehicle),
-							gear = GetVehicleCurrentGear(currentVehicle),
-							abs = (GetVehicleWheelSpeed(currentVehicle, 0) == 0.0) and (vehicleSpeed > 0.0),
-							hBrake = GetVehicleHandbrake(currentVehicle),
+							display  = displayEnabled and IsVehicleEngineOn(currentVehicle),
+							rpm      = GetVehicleCurrentRpm(currentVehicle),
+							gear     = GetVehicleCurrentGear(currentVehicle),
+							abs      = (GetVehicleWheelSpeed(currentVehicle, 0) == 0.0) and (vehicleSpeed > 0.0),
+							hBrake   = GetVehicleHandbrake(currentVehicle),
 							rawSpeed = vehicleSpeed,
 						}
 
@@ -207,7 +308,7 @@ CreateThread(function()
 		Wait(1000)
 	end
 end)
-
+ 
 -- Resource cleanup
 AddEventHandler("onResourceStop", function(resource)
 	if resource == ResourceName then
