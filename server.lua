483f51d5b109041a1baff67eb2cd69f9ce120bde
Akkariin Meiko <akkariins@gmail.com>
2020-12-31 20:59:00 +0800

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,3 +1,5 @@
+RegisterServerEvent('HologramSpeed:CheckTheme')
+
 local resourceName = GetCurrentResourceName()
 local lowerCaseName = string.lower(resourceName)
 if lowerCaseName ~= resourceName then
@@ -5,3 +7,21 @@ if lowerCaseName ~= resourceName then
 else
 	SetConvarReplicated("hsp_defaultTheme", "default")
 end
+
+AddEventHandler('HologramSpeed:CheckTheme', function(theme)
+	local netID = source
+	if theme == 'default' then
+		TriggerClientEvent('HologramSpeed:SetTheme', netID, theme)
+	elseif string.match(theme, '(%w+)') then
+		local path = string.format("%s/ui/css/themes/%s.css", GetResourcePath(GetCurrentResourceName()), theme)
+		local file = io.open(path,"r")
+		if file ~= nil then
+			io.close(file)
+			TriggerClientEvent('HologramSpeed:SetTheme', netID, theme)
+		else
+			TriggerClientEvent('chat:addMessage', netID, {args = {"Cannot find the theme ^1" .. theme .. "^r!"}})
+		end
+	else
+		TriggerClientEvent('chat:addMessage', netID, {args = {"Invalid theme name ^1" .. theme .. "^r!"}})
+	end
+end)
